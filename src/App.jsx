b6f1676eccb7d6087b6c24e93fda1669a311edc3
RyomenDev <aakashvatsh007@gmail.com>
2025-01-12 10:09:03 +0530

    dragon-svg:bug

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DragonAnimation from "./DragonAnimation";
+// import DragonSVG from "./DragonSVG";
+// import DragonSvg from "./utility/Dragon.svg";
+
+const App = () => {
+  return (
+    <div>
+      <DragonAnimation />
+      {/* <DragonSVG /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DragonSVG.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DragonSvg from "./utility/Dragon.svg";
+
+const DragonSVG = () => {
+  return <img src={DragonSvg} alt="Dragon" />;
+};
+
+export default DragonSVG;
